c5c73612b4ac0483d35f4efbb18a9219bebff33b
RZEdward <ross@edwardsnetwork.com>
2023-09-19 16:01:22 +0100

    fix_int_check

--- SQL_CreateTable_Generator.py
+++ SQL_CreateTable_Generator.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 csv_file = 'C:/1Projects/Covid_Data_Exploration/1_Covid_Deaths.csv'
 
@@ -11,11 +10,10 @@ for column in df.columns:
     if df[column].dtype == 'float64':
         column_data_types[column] = 'NUMERIC(16, 3)'
     else:
-        numeric_values = pd.to_numeric(df[column], errors='coerce')
-        max_value = numeric_values.max()
-    
-        if not pd.isnull(max_value) and max_value > 99999999:
-            column_data_types[column] = 'BIGINT'
+        # Check if the column contains integers (no decimals) within valid numeric values
+        is_integer = df[column].apply(lambda x: str(x).replace('.', '', 1).isdigit() if pd.notna(x) else True).all()
+        if is_integer:
+            column_data_types[column] = 'INTEGER'
         else:
             if 'date' in column.lower():
                 column_data_types[column] = 'DATE'
@@ -23,10 +21,14 @@ for column in df.columns:
                 if df[column].dtype == 'object':
                     column_data_types[column] = 'VARCHAR(255)'
                 else:
-                    if pd.to_numeric(df[column], errors='coerce').notna().all():
-                        column_data_types[column] = 'INTEGER'
-                    else:
-                        column_data_types[column] = 'undefined'
+                    column_data_types[column] = 'NUMERIC(16, 3)'
+
+# Check and redefine numeric columns as INTEGER if they primarily contain integers with zero decimals
+for column in column_data_types.keys():
+    if column_data_types[column] == 'NUMERIC(16, 3)':
+        is_integer_column = df[column].apply(lambda x: x % 1 == 0 if pd.notna(x) else True).all()
+        if is_integer_column:
+            column_data_types[column] = 'INTEGER'
 
 create_table_sql = "CREATE TABLE FATALITIES (\n"
 for column, data_type in column_data_types.items():
