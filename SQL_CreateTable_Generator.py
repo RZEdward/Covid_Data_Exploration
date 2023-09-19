25cb46d8e8d94f81d0125f2a9368bb72c23e4228
RZEdward <ross@edwardsnetwork.com>
2023-09-19 20:19:43 +0100

    fix_bigint

--- SQL_CreateTable_Generator.py
+++ SQL_CreateTable_Generator.py
@@ -1,38 +1,43 @@
 import pandas as pd
 
-csv_file = 'C:/1Projects/Covid_Data_Exploration/1_Covid_Deaths.csv'
+csv_file = 'C:/1Projects/Covid_Data_Exploration/1_Covid_Vaccinations.csv'
 
 df = pd.read_csv(csv_file)
 
 column_data_types = {}
 
 for column in df.columns:
-    if df[column].dtype == 'float64':
+    if df[column].dtype == 'float64' or df[column].dtype == 'int64':
         column_data_types[column] = 'NUMERIC(16, 3)'
     else:
-        # Check if the column contains integers (no decimals) within valid numeric values
-        is_integer = df[column].apply(lambda x: str(x).replace('.', '', 1).isdigit() if pd.notna(x) else True).all()
-        if is_integer:
-            column_data_types[column] = 'INTEGER'
+        if 'date' in column.lower():
+            column_data_types[column] = 'DATE'
         else:
-            if 'date' in column.lower():
-                column_data_types[column] = 'DATE'
+            if df[column].dtype == 'object':
+                column_data_types[column] = 'VARCHAR(255)'
             else:
-                if df[column].dtype == 'object':
-                    column_data_types[column] = 'VARCHAR(255)'
-                else:
-                    column_data_types[column] = 'NUMERIC(16, 3)'
+                column_data_types[column] = 'undefined'
 
-# Check and redefine numeric columns as INTEGER if they primarily contain integers with zero decimals
 for column in column_data_types.keys():
     if column_data_types[column] == 'NUMERIC(16, 3)':
         is_integer_column = df[column].apply(lambda x: x % 1 == 0 if pd.notna(x) else True).all()
         if is_integer_column:
-            column_data_types[column] = 'INTEGER'
+            # Check if the maximum and minimum values in the column exceed 9 digits
+            if df[column].max() > 999999999 or df[column].min() < -999999999:
+                column_data_types[column] = 'BIGINT'
+                print(column)
+                print(df[column].max())
+            else:
+                column_data_types[column] = 'INTEGER'
+                
 
-create_table_sql = "CREATE TABLE FATALITIES (\n"
+create_table_sql = "CREATE TABLE VACCINATIONS (\n"
 for column, data_type in column_data_types.items():
     create_table_sql += f"    {column} {data_type},"
 create_table_sql = create_table_sql.rstrip(',\n') + "\n);"
 
 print(create_table_sql)
+
+result = df.dtypes
+
+#print(result)
